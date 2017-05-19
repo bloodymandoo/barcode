a79fc8f54504b55868583f36d7be883f09e585e1
shenxuan <1003711565@qq.com>
2017-05-19 11:52:49 +0800

    提交

---
+++ WinForms/BarcodeGeneration/Matri(2D)Barcodes/QRCode Code/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace BarcodeLibUtilizationInWinForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(103, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(355, 82);
+            this.label1.TabIndex = 1;
+            this.label1.Text = resources.GetString("label1.Text");
+            // 
+            // button1
+            // 
+            this.button1.Image = global::QRCode.Properties.Resources._16;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(363, 114);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Create";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::QRCode.Properties.Resources.barcode;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(78, 82);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(470, 208);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "QR Code";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ WinForms/BarcodeSettingsAndScan/BarcodeSettingsAndScan/CustomControl1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+
+namespace BarcodeLibUtilizationInWinForm
+{
+
+    public partial class myColorComboBox : ComboBox
+    {
+
+        public myColorComboBox()
+        {
+            InitializeComponent();
+            InitItems();
+        }
+        public myColorComboBox(IContainer container)
+        {
+            container.Add(this);
+            InitializeComponent();
+            InitItems();
+        }
+        private void InitItems()
+        {
+            this.DrawMode = DrawMode.OwnerDrawFixed;
+            this.DropDownStyle = ComboBoxStyle.DropDownList;
+            this.Items.Clear();
+            Array allColors = Enum.GetValues(typeof(KnownColor));
+            foreach (KnownColor var in allColors)
+            {
+                this.Items.Add(var.ToString());
+            }
+            this.SelectedIndex = 0;
+        }
+        protected override void OnDrawItem(DrawItemEventArgs e)
+        {
+
+            if (e.Index >= 0)
+            {
+                string colorName = this.Items[e.Index].ToString();
+                SolidBrush brush = new SolidBrush(Color.FromName(colorName));
+                Font font = new Font("宋体", 9);
+                Rectangle rect = e.Bounds;
+                rect.Inflate(-2, -2);
+                Rectangle rectColor = new Rectangle(rect.Location, new Size(20, rect.Height));
+                e.Graphics.FillRectangle(brush, rectColor);
+                e.Graphics.DrawRectangle(Pens.Black, rectColor);
+                e.Graphics.DrawString(colorName, font, Brushes.Black, (rect.X + 22), rect.Y);
+            }
+
+        }
+        public string SelectColorName
+        {
+            get { return this.Text; }
+        }
+        public Color SelectColor
+        {
+            get { return Color.FromName(this.Text); }
+        }
+
+    }
+
+}
